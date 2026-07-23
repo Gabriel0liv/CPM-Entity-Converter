f9841ae6ceff3e8b3ff29700d68db2c4b7936b62
Gabriell0liv <gabrielr.oliveira@ua.pt>
2026-07-24 00:21:41 +0100

    feat(t303): add persisted effective target registry

---
+++ validator-cpm/src/main/java/io/github/gabriel0liv/cpmconverter/validator/CpmPersistedElementTargetV1.java
@@ -0,0 +1,2 @@
+package io.github.gabriel0liv.cpmconverter.validator;
+public record CpmPersistedElementTargetV1(CpmPersistedElementV1 element) implements CpmPersistedTargetV1 {}
---
+++ validator-cpm/src/main/java/io/github/gabriel0liv/cpmconverter/validator/CpmPersistedRootTargetV1.java
@@ -0,0 +1,2 @@
+package io.github.gabriel0liv.cpmconverter.validator;
+public record CpmPersistedRootTargetV1(CpmPersistedRootV1 root) implements CpmPersistedTargetV1 {}
