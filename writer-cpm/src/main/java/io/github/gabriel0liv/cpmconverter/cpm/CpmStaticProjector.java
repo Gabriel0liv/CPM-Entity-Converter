be7465eaa79e09626644662d0d35f2ebac216f96
Gabriel Oliveira <148448623+Gabriel0liv@users.noreply.github.com>
2026-08-26 01:44:51 +0100

    feat: scaffold CPM static projector

---
+++ writer-cpm/src/main/java/io/github/gabriel0liv/cpmconverter/cpm/CpmStaticProjector.java
@@ -0,0 +1,4 @@
+package io.github.gabriel0liv.cpmconverter.cpm;
+
+/** Projects validated ModelIR geometry into a deterministic CPM V1-facing static graph. */
+public final class CpmStaticProjector {}
