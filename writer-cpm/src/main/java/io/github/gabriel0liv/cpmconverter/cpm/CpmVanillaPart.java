3aa9eafcfd9cc17e168eb2ecbcda8c532f05fed3
Gabriel Oliveira <148448623+Gabriel0liv@users.noreply.github.com>
2026-08-26 01:41:54 +0100

    feat: add CPM static projection graph types

---
+++ writer-cpm/src/main/java/io/github/gabriel0liv/cpmconverter/cpm/CpmVanillaPart.java
@@ -0,0 +1,11 @@
+package io.github.gabriel0liv.cpmconverter.cpm;
+
+/** Vanilla CPM roots in deterministic project order. */
+public enum CpmVanillaPart {
+  HEAD,
+  BODY,
+  LEFT_ARM,
+  RIGHT_ARM,
+  LEFT_LEG,
+  RIGHT_LEG
+}
